bf30d8d65853915167879f0d7f3712400932d04d
James Douglass <jdouglass@stanford.edu>
2022-11-14 19:26:51 -0800

    Adding a script to download all SRTM tiles.

---
+++ srtm-data/download_all_tiles.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#SBATCH --time=4:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jdouglass@stanford.edu
+#SBATCH --partition=hns,normal
+#SBATCH --job-name="Download SRTM tiles"
+#SBATCH --output=/scratch/users/jadoug06/slurm-logfiles/slurm-%j.%x.out
+#
+# --partition=hns,normal means that this will be submitted to both queues, whichever gets to it first will be used.
+
+
+set -e
+set -x
+
+WORKING_DIR=srtm-global-30m
+
+if [[ -n ${SHERLOCK+x} ]]
+then
+    # execute on $SCRATCH if we're on sherlock
+    WORKING_DIR="$SCRATCH/$WORKING_DIR"
+fi
+
+mkdir $WORKING_DIR || echo "$WORKING_DIR already exists"
+
+cat srtm-data/srtm30m_urls.txt | parallel -j 8 "wget --directory-prefix=$WORKING_DIR {}"
