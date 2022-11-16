c604cf76e9737e4ee5d89f953864292adad89a17
James Douglass <jdouglass@stanford.edu>
2022-11-16 14:48:22 -0800

    Also building overviews.

--- srtm-local-1s-v3.py
+++ srtm-local-1s-v3.py
@@ -1,6 +1,8 @@
 import json
 import logging
+import math
 import os
+import time
 import zipfile
 
 import pygeoprocessing
@@ -38,6 +40,44 @@ DEFAULT_GTIFF_CREATION_TUPLE_OPTIONS = ('GTIFF', (
 #     download everything
 #     create a VRT
 #     do a raster_calculator call if needed
+
+
+def build_overviews(raster_path, internal=False,
+                    resampling_method=gdal.GRA_NearestNeighbour):
+    open_flags = gdal.OF_RASTER
+    if internal:
+        open_flags |= gdal.GA_Update
+        LOGGER.info(f"Building internal overviews on {raster_path}")
+    else:
+        LOGGER.info("Building external overviews.")
+    raster = gdal.OpenEx(raster_path, open_flags)
+    n_pixels_x = raster.RasterXSize
+    n_pixels_y = raster.RasterYSize
+
+    # This loop and limiting factor borrowed from gdaladdo.cpp
+    overview_scales = []
+    factor = 2
+    limiting_factor = 256
+    while (math.ceil(n_pixels_x / factor) > limiting_factor or
+           math.ceil(n_pixels_y / factor) > limiting_factor):
+        overview_scales.append(factor)
+        factor *= 2
+
+    def overviews_progress(*args, **kwargs):
+        pct_complete, name, other = args
+        percent = round(pct_complete * 100, 2)
+        if time.time() - overviews_progress.last_progress_report > 5.0:
+            LOGGER.info(f"Overviews progress: {percent}%")
+            overviews_progress.last_progress_report = time.time()
+    overviews_progress.last_progress_report = time.time()
+
+    LOGGER.debug(f"Using overviews {overview_scales}")
+    result = raster.BuildOverviews('NEAREST', overviewlist=overview_scales,
+                                   callback=overviews_progress)
+    LOGGER.info(f"Overviews completed for {raster_path}")
+    if result:  # Result will be nonzero on error.
+        raise RuntimeError(
+            f"Building overviews failed or was interrupted for {raster_path}")
 
 
 def _identity(matrix):
@@ -103,6 +143,7 @@ def main(bbox, cache_dir, target_vrt, target_gtiff):
         'raster_driver_creation_tuple': DEFAULT_GTIFF_CREATION_TUPLE_OPTIONS,
         'largest_block': 2**18,  # 4x the size of default pgp blocksize
     })
+    build_overviews(target_gtiff, internal=True)
 
 
 if __name__ == '__main__':
