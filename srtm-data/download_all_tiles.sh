937b5e45d7b451201c6c4187b1cb67eccf620dfd
James Douglass <jdouglass@stanford.edu>
2022-11-15 19:05:21 -0800

    Refactor of the gdalinfo loop.
    This refactor includes:
    * use of /vsizip/, which is needed for a small number of the tiles.
    * loading of needed tools when on sherlock
    * some additional comments.

--- srtm-data/download_all_tiles.sh
+++ srtm-data/download_all_tiles.sh
@@ -24,17 +24,34 @@ if [[ -n ${SHERLOCK+x} ]]
 then
     # execute on $SCRATCH if we're on sherlock
     WORKING_DIR="$SCRATCH/$WORKING_DIR"
+    module load physics gdal/3.5.2
+    module load jq
 fi
 
 mkdir $WORKING_DIR || echo "$WORKING_DIR already exists"
 
+# Runtime: This took about 2 hours to run on 4 CPUs
 cat srtm30m_urls.txt | parallel -j 8 --retries 3 "wget --no-clobber --no-verbose --user='$NASA_EARTHDATA_USERNAME' --password='$NASA_EARTHDATA_PASSWORD' --directory-prefix=$WORKING_DIR {}"
 
+# Runtime: with all of the SRTM files downloaded, this took just under 3 hours to run.
+# It was on SCRATCH, so I'm guessing that the disk I/O is the bottleneck.
 SRTM_JSON_FILE="srtm_bboxes.json"
+rm $SRTM_JSON_FILE || echo "Can't delete $SRTM_JSON_FILE if it doesn't exist!"
 echo "{" >> $SRTM_JSON_FILE
+
+# This loop has so many iterations that we don't really need everything logged.
+# We're still exiting on the first nonzero exit code.
+set +x
+
+# $srtm_file is an absolute path in this case.
 for srtm_file in $(find $WORKING_DIR -name "*.hgt.zip")
 do
-    echo "\"$(basename $srtm_file)\": $(gdalinfo -json $srtm_file | jq -c '.wgs84Extent.coordinates[0]')," >> $SRTM_JSON_FILE
+    echo $srtm_file
+    SRTM_BASENAME=$(echo $srtm_file | sed 's|.zip||g')
+    # using /vsizip/ is more reliable; some (about 17) SRTM tiles will only
+    # open with /vsizip/.
+    RASTER_METADATA=$(gdalinfo -json /vsizip/$srtm_file/$SRTM_BASENAME)
+    echo "\"$(basename $srtm_file)\": $(echo $RASTER_METADATA | jq -c '.wgs84Extent.coordinates[0]')," >> $SRTM_JSON_FILE
 done
 echo "}" >> $SRTM_JSON_FILE
 
