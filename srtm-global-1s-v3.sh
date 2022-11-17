128af56f247636fff092cb3a22c5e76444e70bcb
James Douglass <jdouglass@stanford.edu>
2022-11-17 15:43:49 -0800

    Why not try gdal_translate instead?

--- srtm-global-1s-v3.sh
+++ srtm-global-1s-v3.sh
@@ -21,6 +21,7 @@ if [[ -n ${SHERLOCK+x} ]]
 then
     # execute on $SCRATCH if we're on sherlock
     WORKING_DIR="$SCRATCH/$WORKING_DIR"
+    module load physics gdal/3.5.2
 fi
 
 mkdir $WORKING_DIR || echo "$WORKING_DIR already exists"
@@ -32,13 +33,25 @@ DIGEST=sha256:acdae8dc64e1c7f31e6d2a1f92aa16d1f49c50d58adcd841ee2d325a96de89d9
 CACHE="$L_SCRATCH"
 find "$SCRATCH/srtm-global-30m" -name "*.zip" | parallel -j 8 "unzip -d $CACHE {}"
 
-GTIFF_PATH="$L_SCRATCH/srtm-global-1s-v3.tif"
-singularity run \
-    docker://$CONTAINER@$DIGEST \
-    python "srtm-local-1s-v3.py" \
-        --extent="global" \
-        --cache-dir="$CACHE" \
-        --vrt-path="$WORKING_DIR/global.vrt" \
-        --gtiff-path="$GTIFF_PATH"
+VRT_PATH="$WORKING_DIR/cmdline-global.vrt"
+gdalbuildvrt $VRT_PATH $(find $CACHE -name "*.hgt")
 
-rsync --progress "$GTIFF_PATH" "$WORKING_DIR/srtm-global-1s-v3.tif"
+GTIFF_PATH="$L_SCRATCH/srtm-global-1s-v3.tif"
+gdal_translate \
+    -co "COMPRESS=LZW" \
+    -co "TILED=YES" \
+    "$VRT_PATH" \
+    "$GTIFF_PATH"
+
+gdaladdo $GTIFF_PATH
+rsync --progress "$GTIFF_PATH" "$WORKING_DIR/cmdline-srtm-global-1s-v3.tif"
+
+#singularity run \
+#    docker://$CONTAINER@$DIGEST \
+#    python "srtm-local-1s-v3.py" \
+#        --extent="global" \
+#        --cache-dir="$CACHE" \
+#        --vrt-path="$WORKING_DIR/global.vrt" \
+#        --gtiff-path="$GTIFF_PATH"
+
+#rsync --progress "$GTIFF_PATH" "$WORKING_DIR/cmdline-srtm-global-1s-v3.tif"
