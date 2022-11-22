4f9ec9e781e3bc1cd51b31a4e804e822bd2326cc
James Douglass <jdouglass@stanford.edu>
2022-11-22 11:23:00 -0800

    Adding a script to convert the SRTM gtiff to tiles.

---
+++ srtm-convert-global-to-tiles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jdouglass@stanford.edu
+#SBATCH --partition=hns,normal
+#SBATCH --job-name="srtm-convert-global-to-tiles"
+#SBATCH --output=/scratch/users/jadoug06/slurm-logfiles/slurm-%j.%x.out
+#
+# --partition=hns,normal means that this will be submitted to both queues, whichever gets to it first will be used.
+
+set -e
+set -x
+
+WORKING_DIR=srtm-global-1s-v3-tiles
+
+CONTAINER=ghcr.io/natcap/devstack
+DIGEST=sha256:54066e72aa135deb8e2f60fda2f42f1856912e36967446659ad754b4b64d7efa
+
+# gdal2tiles is not available in the standard Sherlock gdal package, so here
+# we'll run it in the devstack container.
+singularity run \
+    --env GDAL_CACHEMAX=1024 \
+    docker://$CONTAINER@$DIGEST \
+    gdal2tiles.py \
+        --webviewer=leaflet \
+        --title="SRTMv3 1 Arc-Second" \
+        --zoom="2-5" \
+        "$SCRATCH/srtm-global-1s-v3-BACKUP/srtm-global-1s-v3.tif" \
+        "$SCRATCH/$WORKING_DIR"
