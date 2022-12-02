7b64dfb46d2e2cb86e037b75ad998ade78eed352
James Douglass <jdouglass@stanford.edu>
2022-12-01 16:33:31 -0800

    Revising how the gmted2010 json file is built.

--- gmted2010/build-product-json.py
+++ gmted2010/build-product-json.py
@@ -9,17 +9,25 @@ logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 json_data = {}
-for gmted_url in open(sys.argv[1]):
-    filename = os.path.basename(gmted_url)
-    try:
-        LOGGER.info(f"Getting info for {gmted_url}")
-        raster = gdal.Open(f'/vsicurl/{gmted_url}')
-        raster_info = gdal.Info(raster, options=['-json'])
-        bbox = raster_info['wgs84Extent']['coordinates'][0]
-        json_data[filename] = bbox
-    finally:
-        raster = None
+counter = 0
+with open(sys.argv[1]) as gmted_url_file:
+    for line in gmted_url_file:
+        gmted_url = line.strip()
+        filename = os.path.basename(gmted_url)
+        try:
+            gdal_vsi_url = f'/vsicurl/{gmted_url}'
+            LOGGER.info(f"Getting info for {gdal_vsi_url}")
+            raster = gdal.Open(gdal_vsi_url)
+            raster_info = gdal.Info(raster, options=['-json'])
+            bbox = raster_info['wgs84Extent']['coordinates'][0]
+            json_data[filename] = bbox
+        finally:
+            raster = None
+
+        counter += 1
+        if counter > 1:
+            break
 
 
 with open('gmted2010.json', 'w') as info_json:
-    json.dump(json_data, info_json, indent=4)
+    json.dump(json_data, info_json)
